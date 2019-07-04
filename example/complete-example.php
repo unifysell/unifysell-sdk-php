613e66869d8a27c0a689610087988a95cc29282d
Thomas Rückert <thomas.rueckert@s2010.tu-chemnitz.de>
2019-07-04 10:33:39 +0200

    updated (re-generated) sdk for newest openapi version * only payments api available now

--- example/complete-example.php
+++ example/complete-example.php
@@ -3,20 +3,24 @@
 require 'vendor/autoload.php';
 
 # SDK - access the API
-$config = Unifysell\SDK\Configuration::getDefaultConfiguration()->setApiKey(
+$config = Unifysell\SDK\Configuration::getDefaultConfiguration();
+$config->setApiKey(
     'Authorization',
     '----INSERT BEARER TOKEN HERE----'
 );
 $config->setApiKeyPrefix('Authorization', 'Bearer');
 
-$apiInstance = new Unifysell\SDK\Api\OrdersApi(
+$apiInstance = new Unifysell\SDK\Api\PaymentsApi(
     new GuzzleHttp\Client(),
     $config
 );
+
+$apiInstance->getConfig();
+
 $id = 1; // int | Order ID
 
 try {
-    $result = $apiInstance->getOrder(1);
+    $result = $apiInstance->getPaymentV1(1);
     print_r($result);
 } catch (Exception $e) {
     echo 'Exception when calling OrdersApi->getOrder: ', $e->getMessage(), PHP_EOL;
--- src/unifysell-api-sdk/docs/Model/OrderMarkAsShippedRequest.md
+++
@@ -1,9 +0,0 @@
-# OrderMarkAsShippedRequest
-
-## Properties
-Name | Type | Description | Notes
------------- | ------------- | ------------- | -------------
-**label_id** | **int** | Give the id of a label to link shipment information with the order | 
-
-[[Back to Model list]](../../README.md#documentation-for-models) [[Back to API list]](../../README.md#documentation-for-api-endpoints) [[Back to README]](../../README.md)
-
